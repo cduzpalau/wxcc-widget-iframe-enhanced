c20c178a545194a1d60296d9d93e2af7eed9eb42
Carles Duz Palau <carles.duz@gmail.com>
2025-07-23 15:26:29 +0100

    debug mode

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ import { Desktop } from "@wxcc-desktop/sdk";
 
 // Define a logger for debug/info output
 const logger = Desktop.logger.createLogger("wxcc-enhanced-iframe-logger");
-let debugMode = false;
+let debugMode = true;
 
 class IframeWagentSDK extends HTMLElement {
   constructor() {
